e9aeaad4c9ce4fc0c158076070a484ce35e35f00
Alexey <ra97@mail.ru>
2020-05-22 15:23:02 +0700

    1

--- Laba8/Laba8/Form1.Designer.cs
+++ Laba8/Laba8/Form1.Designer.cs
@@ -28,13 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.imagePictureBox = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(12, 236);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 0;
+            this.buttonLoad.Text = "Load image";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(93, 236);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save image";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // imagePictureBox
+            // 
+            this.imagePictureBox.Location = new System.Drawing.Point(12, 12);
+            this.imagePictureBox.Name = "imagePictureBox";
+            this.imagePictureBox.Size = new System.Drawing.Size(347, 218);
+            this.imagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imagePictureBox.TabIndex = 2;
+            this.imagePictureBox.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 265);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(351, 53);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(203, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Hide";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(284, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Extract";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(371, 330);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.imagePictureBox);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonLoad);
+            this.Name = "Form1";
+            this.Text = "Laba8 LSB-replacement";
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.PictureBox imagePictureBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Laba8/Laba8/Form1.cs
+++ Laba8/Laba8/Form1.cs
@@ -8,13 +8,210 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Drawing.Imaging;
+
+
 namespace Laba8
 {
     public partial class Form1 : Form
     {
+        private Bitmap bmp = null;
+        private string extractedText = string.Empty;
+        public enum State
+        {
+            Hiding,
+            Filling_With_Zeros
+        };
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Image Files (*.jpeg; *.png; *.bmp)|*.jpg; *.png; *.bmp";
+
+            if (open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                imagePictureBox.Image = Image.FromFile(open_dialog.FileName);
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save_dialog = new SaveFileDialog();
+            save_dialog.Filter = "Png Image|*.png|Bitmap Image|*.bmp";
+            if (save_dialog.ShowDialog() == DialogResult.OK)
+            {
+                switch (save_dialog.FilterIndex)
+                {
+                    case 0:
+                        {
+                            bmp.Save(save_dialog.FileName, ImageFormat.Png);
+                        }
+                        break;
+                    case 1:
+                        {
+                            bmp.Save(save_dialog.FileName, ImageFormat.Bmp);
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bmp = (Bitmap)imagePictureBox.Image;
+            string text = textBox1.Text;
+            if (text.Equals("")) { MessageBox.Show("Текст не может быть пустым"); return; }
+            bmp = EmbedText(text, bmp);
+            MessageBox.Show("Успешно");
+        }
+
+        public static Bitmap EmbedText(string text, Bitmap bmp)
+        {
+            State state = State.Hiding;
+            int charIndex = 0, charValue = 0, zeros = 0;
+            long pixelElementIndex = 0;
+            int R = 0, G = 0, B = 0;
+
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+                    R = pixel.R - pixel.R % 2;
+                    G = pixel.G - pixel.G % 2;
+                    B = pixel.B - pixel.B % 2;
+                    for (int n = 0; n < 3; n++)
+                    {
+                        if (pixelElementIndex % 8 == 0)
+                        {
+                            if (state == State.Filling_With_Zeros && zeros == 8)
+                            {
+                                if ((pixelElementIndex - 1) % 3 < 2)
+                                {
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+                                return bmp;
+                            }
+
+                            if (charIndex >= text.Length)
+                            {
+                                state = State.Filling_With_Zeros;
+                            }
+                            else
+                            {
+                                charValue = text[charIndex++];
+                            }
+                        }
+                        switch (pixelElementIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        R += charValue % 2;
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 1:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        G += charValue % 2;
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 2:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        B += charValue % 2;
+                                        charValue /= 2;
+                                    }
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+                                break;
+                        }
+                        pixelElementIndex++;
+                        if (state == State.Filling_With_Zeros)
+                        {
+                            zeros++;
+                        }
+                    }
+                }
+            }
+            return bmp;
+        }
+
+        public static string ExtractText(Bitmap bmp)
+        {
+            int colorUnitIndex = 0;
+            int charValue = 0;
+            string extractedText = String.Empty;
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+                    for (int n = 0; n < 3; n++)
+                    {
+                        switch (colorUnitIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    charValue = charValue * 2 + pixel.R % 2;
+                                }
+                                break;
+                            case 1:
+                                {
+                                    charValue = charValue * 2 + pixel.G % 2;
+                                }
+                                break;
+                            case 2:
+                                {
+                                    charValue = charValue * 2 + pixel.B % 2;
+                                }
+                                break;
+                        }
+                        colorUnitIndex++;
+                        if (colorUnitIndex % 8 == 0)
+                        {
+                            charValue = reverseBits(charValue);
+                            if (charValue == 0)
+                            {
+                                return extractedText;
+                            }
+                            char c = (char)charValue;
+                            extractedText += c.ToString();
+                        }
+                    }
+                }
+            }
+
+            return extractedText;
+        }
+        public static int reverseBits(int n)
+        {
+            int result = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+                n /= 2;
+            }
+            return result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bmp = (Bitmap)imagePictureBox.Image;
+            string extractedText = ExtractText(bmp);
+
+            textBox1.Text = extractedText;
         }
     }
 }
